8bc4063d9d237f51cfe2506e3637a078b839fea2
Tanya <lamara@tut.by>
2022-12-16 16:44:07 +0100

    refactor: change code on the simpler one

--- src/fs/upDirectory.js
+++ src/fs/upDirectory.js
@@ -1,9 +1,5 @@
 import path from 'path';
 
 export default async function upDirecory(filepath) {
-    let pathArray = filepath.split(path.sep);
-    
-    if (pathArray.length > 1) pathArray.pop();
-
-    return pathArray.length > 1 ? pathArray.join(path.sep) : pathArray + path.sep;
-};
+    return path.resolve(filepath, '..');
+}
